42f510e446ac576992b68191703cb3f3485dbffc
Binh Nguyen <binh.nt@fiberblock.io>
2022-04-04 15:06:49 +0700

    init source

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ConnectComponent from "@/components/ConnectComponent.vue";
+
+const show = ref(false);
+const onError = (error: Error) => {
+  console.debug({ error });
+};
+</script>
+<template>
+  <div>
+    <button @click="show = true">Open modal connect</button>
+  </div>
+  <ConnectComponent @error="onError" :chain="250" v-model="show" />
+</template>
